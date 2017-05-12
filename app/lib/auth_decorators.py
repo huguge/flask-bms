30e9c47f1aaa128317f86917d29e64f1450a4fa4
zhangmingkai <zhangmingkai@cnnic.cn>
2017-05-12 08:57:28 +0800

    add lib code

---
+++ app/lib/auth_decorators.py
@@ -0,0 +1,22 @@
+from functools import wraps
+from flask import abort
+from flask_login import current_user
+
+from app.models import Permission
+
+def permission_require(permission):
+    def wrapper(f):
+        @wraps(f)
+        def decorator_func(*args,**kw):
+            if not current_user.can(permission):
+                # print '403'
+                abort(403)
+            return f(*args,**kw)
+        return decorator_func
+    return wrapper
+
+def super_admin_require(f):
+    return permission_require(Permission.ADMIN_USER)(f)
+def content_admin_require(f):
+    return permission_require(Permission.ADMIN_CONTENT)(f)
+
